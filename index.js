066c697128cf29be5460d5e37ef6b0899edc895e
Abdón Rodríguez Davila <a@abdonrd.com>
2020-04-17 08:56:05 +0200

    Add license header

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) IBM, Corp. and its affiliates.
+ *
+ * This source code is licensed under the Apache license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 'use strict';
 
 const ibm = require('ibm-cos-sdk');
